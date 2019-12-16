4dba015cc90575593564b5cd1ba8fd928afef692
Tom Anderson <dante.renevo@gmail.com>
2019-12-16 13:19:35 -0800

    Updating to application

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/portcullis/service-example
 go 1.13
 
 require (
+	github.com/portcullis/application v0.0.0-20191216211626-a70f0d307f3f
 	github.com/portcullis/logging v0.0.0-20191216195124-dbd89d13ff83
 	github.com/portcullis/module v0.0.0-20191216195407-4a3750be7c0f
 )
--- main.go
+++ main.go
@@ -1,50 +1,16 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
-
-	"github.com/portcullis/logging"
-	"github.com/portcullis/module"
+	"github.com/portcullis/application"
 	"github.com/portcullis/service-example/internal/modules/moduledump"
 	"github.com/portcullis/service-example/internal/modules/simple"
 )
 
 func main() {
-	ctr := module.Controller{}
-	ctr.Add("Simple", simple.New())
-	ctr.Add("Dumper", moduledump.New())
-
-	schan := make(chan os.Signal, 1)
-	defer close(schan)
-
-	signal.Notify(schan, os.Interrupt, syscall.SIGTERM)
-	defer signal.Stop(schan)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go func() {
-		if err := ctr.Run(ctx); err != nil && err != context.Canceled {
-			logging.Error("Failed to run: %v", err)
-		}
-		wg.Done()
-		cancel()
-	}()
-
-	select {
-	case <-ctx.Done():
-
-	case sig := <-schan:
-		logging.Info("Signal %v received", sig)
-		cancel()
+	if err := application.Run("sample", "v0.0.0",
+		application.Module("Simple", simple.New()),
+		application.Module("Dumper", moduledump.New()),
+	); err != nil {
+		panic(err)
 	}
-
-	wg.Wait()
 }
