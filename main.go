0c6c801c973314783efaffea02b6924bc60a2853
Tom Anderson <dante.renevo@gmail.com>
2020-07-18 14:33:17 -0700

    Commenting out module failer
    This makes application panic on shutdown

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/portcullis/application"
-	"github.com/portcullis/service-example/internal/modules/failer"
 	"github.com/portcullis/service-example/internal/modules/moduledump"
 	"github.com/portcullis/service-example/internal/modules/simple"
 )
@@ -10,7 +9,7 @@ import (
 func main() {
 	if err := application.Run("sample", "v0.0.0",
 		application.Module("Dumper", moduledump.New()),
-		application.Module("Failer", failer.New()),
+		//application.Module("Failer", failer.New()),
 		application.Module("Simple", simple.New()),
 	); err != nil {
 		panic(err)
