df2f997c58fd78e49da5377896a4b365fcf6c626
Tom Anderson <dante.renevo@gmail.com>
2020-09-18 22:06:18 -0700

    Updated service-example with latest application bootstrapper

--- go.mod
+++ go.mod
@@ -1,10 +1,9 @@
 module github.com/portcullis/service-example
 
-go 1.14
+go 1.15
 
 require (
-	github.com/portcullis/application v0.0.0-20200718180734-6cb655dce824
-	github.com/portcullis/config v0.0.0-20200718224424-ef05468df8d5
+	github.com/portcullis/application v0.0.0-20200919044530-9c92edb47552
 	github.com/portcullis/logging v0.0.0-20200718224534-0e694ad7477a
-	github.com/portcullis/module v0.0.0-20200718180813-ef47b4a94c31
+	github.com/portcullis/module v0.0.0-20200919045418-3f460d896606
 )
--- main.go
+++ main.go
@@ -1,54 +1,20 @@
 package main
 
 import (
-	"flag"
-	"os"
+	"context"
 
 	"github.com/portcullis/application"
-	"github.com/portcullis/config"
-	"github.com/portcullis/logging"
-	writer "github.com/portcullis/logging/format/simple"
+	"github.com/portcullis/service-example/internal/modules/failer"
 	"github.com/portcullis/service-example/internal/modules/moduledump"
 	"github.com/portcullis/service-example/internal/modules/simple"
 )
 
-func init() {
-	// global logging setup
-	level := logging.LevelInformational
-	simpleWriter := writer.New(
-		os.Stdout,
-		writer.Level(level),
-	)
-	logging.DefaultLog = logging.New(
-		logging.WithWriter(simpleWriter),
-	)
-
-	// create a setting
-	levelSetting := &config.Setting{
-		Name:         "Level",
-		Path:         "Logging",
-		Description:  "Logging level to output",
-		DefaultValue: level.String(),
-		Value:        &level,
-	}
-
-	// update the current log level
-	levelSetting.Notify(config.NotifyFunc(func(s *config.Setting) {
-		writer.Level(level)(simpleWriter)
-	}))
-
-	// make it a flag -level
-	levelSetting.Flag("level", flag.CommandLine)
-}
-
 func main() {
-	flag.Parse()
-
-	if err := application.Run("sample", "v0.0.0",
+	if err := application.Bootstrap("sample", "0.0.0",
 		application.Module("Dumper", moduledump.New()),
-		//application.Module("Failer", failer.New()),
+		application.Module("Failer", failer.New()),
 		application.Module("Simple", simple.New()),
-	); err != nil {
+	).Run(context.Background()); err != nil {
 		panic(err)
 	}
 }
